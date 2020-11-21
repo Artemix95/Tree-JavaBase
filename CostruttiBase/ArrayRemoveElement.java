dd228f3f9851012038510846ee38aa7297eec358
Artemix95 <sfrasson95@gmail.com>
2020-11-21 10:27:44 +0100

    arrayRemoveElement_fixed

--- CostruttiBase/ArrayRemoveElement.java
+++ CostruttiBase/ArrayRemoveElement.java
@@ -6,22 +6,26 @@ import java.util.Arrays;
 
 class ArrayRemoveElement {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 6, 3, 8, 4, 9, 11, 8, 10};
+        int[] arr = {10, 1, 10, 10, 7, 10, 10, 10, 10, 10, 2, 5, 10, 10};
         int n = 10;
-        int index = 0;
 
-        //elimino il valore
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == n) {
-                arr[i] = 0;
-                index = i;
-                break;
+            if ((arr[i] == n) || (arr[i - 1] == n)) { //controlla che il valore corrente o il precedente siano uguali all'elemento dal cancellare
+                if (arr[i] != arr[0] && arr[i - 1] == n) { //controlla che non sia in prima posizione e che il valore precedente sia uguale a quello da cancellare
+                    i--;
+                }
+
+                for (int j = i; j < arr.length-1; j++) { //da quel valore in poi sposta tutti i valori di un posto a sx e lascia l'ultimo a zero
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = 0;
+
+                }
+
             }
+
         }
-        //sposto tutti gli altri valori di un posto
-        for (int i = index; i < arr.length - 1; i++) {
-            arr[index] = arr[index + 1];
-        }
+
+
         System.out.println(Arrays.toString(arr));
 
     }
